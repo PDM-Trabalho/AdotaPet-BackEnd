667f02d77aed24a7726d0588576b79c4142388f0
ivesfg1 <wigzfurtado@gmail.com>
2023-12-12 21:32:54 -0300

    removendo gis

--- apps/authentication/api/serializers.py
+++ apps/authentication/api/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from rest_framework_gis.serializers import GeoFeatureModelSerializer
+# from rest_framework_gis.serializers import GeoFeatureModelSerializer
 
 from django.contrib.auth import get_user_model
 
